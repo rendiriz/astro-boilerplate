31e87d7076cce93f048b32a4a17d5439a4dc5f0d
Rendi Rizki <rendirizkir@gmail.com>
2022-09-28 23:04:28 +0700

    ci: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import deno from '@astrojs/deno';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: deno(),
-  integrations: [tailwind(), react()]
+  adapter: vercel(),
+  integrations: [react(), tailwind()],
 });
